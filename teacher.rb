6d4f8071d437698308001cd97fe29a265575fd54
ciraganenicole <ciraganenicole@gmail.com>
2022-06-14 02:32:15 +0200

    Fix linters errors

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
-require "./person.rb"
+require './person'
 
 class Teacher < Person
-    def initialize (specialization, age, name = 'Unknown', parent_permission: true)
-        super(age, name, parent_permission)
-        @specialization = specialization
-    end
-     
-    def can_use_services?
-        true
-    end
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
 end
