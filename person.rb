a58c8f8dea136d4d46634f3c15501d24ee389318
ciraganenicole <ciraganenicole@gmail.com>
2022-06-15 09:02:34 +0200

    Turn Person class into Nameable

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   attr_accessor :name, :age
@@ -18,4 +21,15 @@ class Person
   def can_use_services?
     return true if of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+capitalized_trimmed_person.correct_name
