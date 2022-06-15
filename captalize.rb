4e5d322c006f7768a5a5f371fd082194f371c9ee
ciraganenicole <ciraganenicole@gmail.com>
2022-06-15 09:03:15 +0200

    Capitalize the output

---
+++ captalize.rb
@@ -0,0 +1,12 @@
+require './decorator'
+
+class CapitalizeDecorator < BaseDecorator
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
