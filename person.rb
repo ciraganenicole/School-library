46bf3dbcc8672c008110005343ef299b2160b295
ciraganenicole <ciraganenicole@gmail.com>
2022-06-16 09:40:00 +0200

    Create user interaction

--- person.rb
+++ person.rb
@@ -31,10 +31,3 @@ class Person < Nameable
     Rental.new(date, book, self)
   end
 end
-
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-capitalized_trimmed_person.correct_name
--- trimmer.rb
+++ trimmer.rb
@@ -1,12 +1,8 @@
 require './decorator'
 
-class TrimmerDecorator < BaseDecorator
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
+class TrimmerDecorator
+  def correct_name(name)
+    name = name[0...10] if name.length > 10
+    name.capitalize
   end
 end
